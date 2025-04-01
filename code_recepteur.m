8119b6ed395e13f2a43ec71d541087f417c25adb
Raphael DEBACHE <rdebache@enseirb-matmeca.fr>
2025-04-01 15:26:46 +0200

    estimation de TS ok estimation de rolloff ok

--- code_recepteur.m
+++ code_recepteur.m
@@ -19,9 +19,9 @@ window = hamming(1024);
 noverlap = length(window) / 10;
 
 % Periodogramme de Welch du signal_recu
-[Px, F] = pwelch(signal_recu, window, noverlap, Nfft, 1);
+[Px, F] = pwelch(signal_recu, window, noverlap, Nfft, 1, 'centered');
 
-Px_db = 10*log10(fftshift(Px));
+Px_db = 10*log10(Px);
 
 % sigma = 2; % Écart-type du filtre gaussien
 % windowSize = 10000;
@@ -30,34 +30,34 @@ Px_db = 10*log10(fftshift(Px));
 
 % Affichage du periodogramme
 figure;
-hold on
-plot(F, Px_db_2);
 plot(F, Px_db);
 xlabel('Fréquence (Hz)');
 ylabel('Densité spectrale de puissance (dB/Hz)');
 title('Periodogramme de Welch du signal reçu');
 grid on;
-hold off
 
-% Estimation du Temps Symbole
-Ts = 10^(-11/10);
 
 % Estimation du rolloff
 
-% Détection de la bande à -40 dB
-threshold = - 40; % Seuil à -40 dB
+% Détection de la bande à -13 dB
+threshold = - 13; % Seuil à -13 dB
 indices = find(Px_db >= threshold); % Indices des fréquences au-dessus du seuil
 
 f_min = F(indices(1));
 f_max = F(indices(end));
-B_db = f_max - f_min
+B_Ts = f_max - f_min;
+
+Ts = 1/B_Ts
+% Détection de la bande à -40 dB
+threshold_2 = - 55; % Seuil à -40 dB
+indices_2 = find(Px_db >= threshold_2); % Indices des fréquences au-dessus du seuil
+f_min_2 = F(indices_2(1));
+f_max_2 = F(indices_2(end));
+B_w = f_max_2 - f_min_2
+
+rolloff =  B_w*Ts-1
 
 
-rolloff =  B_db*Ts-1;
 
-h = rcosdesign(rolloff, Nfft, 1);
-H = fftshift(fft(h, Nfft));
-plot(H);
-
-
+scatterplot(signal_recu(1:200))
 
